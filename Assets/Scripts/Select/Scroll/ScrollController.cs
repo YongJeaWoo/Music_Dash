b6eed5d197618ffdf49a544e620aa49af38c34a1
YongJeaWoo <99638097+YongJeaWoo@users.noreply.github.com>
2023-04-02 12:53:14 +0900

    feat : scroll controller
    Scrolling using DTT assets

---
+++ Assets/Scripts/Select/Scroll/ScrollController.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+namespace DTT.InfiniteScroll
+{
+    public class ScrollController : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject musicPrefab;
+
+        [SerializeField]
+        private Transform contentTransform;
+
+        private InfiniteScroll infiniteScroll;
+
+        private int selectIndex = 7;
+
+        private void Start()
+        {
+            CheckInit();
+        }
+
+        private void Update()
+        {
+            InputKeys();
+        }
+
+        private void CheckInit()
+        {
+            infiniteScroll = GetComponent<InfiniteScroll>();
+
+            if (musicPrefab == null) return;
+
+            for (int i = 0; i < selectIndex; i++)
+            {
+                var obj = Instantiate(musicPrefab);
+                obj.transform.SetParent(contentTransform);
+            }
+        }
+
+        private void InputKeys()
+        {
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                ScrollDown();
+            }
+
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                ScrollUp();
+            }
+        }
+
+        private void ScrollUp()
+        {
+            infiniteScroll.Next();
+        }
+
+        private void ScrollDown()
+        {
+            infiniteScroll.Previous();
+        }
+    }
+}
